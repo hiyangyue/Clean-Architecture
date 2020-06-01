74953d0db23a590f963118f6fc1f7d9e6cf76d36
yangyue <hi.yangyue1993@gmail.com>
2020-06-01 11:49:40 +0800

    refactor: extract app module dependencies to buildSrc

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,7 +1,25 @@
 object Version {
-    val glide = "4.11.0"
+    const val glide = "4.11.0"
+    const val constraintlayout = "1.1.3"
+    const val junit = "4.12"
+    const val androidx_junit = "1.1.1"
+    const val espresso_core = "3.2.0"
+    const val appcompat = "1.1.0"
+    const val core_ktx = "1.2.0"
+    const val kotlin = "1.3.71"
 }
 
 object Libs {
-    val glideLibrary = "com.github.bumptech.glide:glide:${Version.glide}"
+    const val glide = "com.github.bumptech.glide:glide:${Version.glide}"
+    const val constraintlayout =
+        "androidx.constraintlayout:constraintlayout:${Version.constraintlayout}"
+    const val junit = "junit:junit:${Version.junit}"
+    const val androidx_junit = "androidx.test.ext:junit:${Version.androidx_junit}"
+    const val espresso_core = "androidx.test.espresso:espresso-core:${Version.espresso_core}"
+    const val appcompact = "androidx.appcompat:appcompat:${Version.appcompat}"
+    const val core_ktx = "androidx.core:core-ktx:${Version.core_ktx}"
+
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Version.kotlin}"
+
+
 }
